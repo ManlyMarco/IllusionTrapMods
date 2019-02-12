b4c1e50edbbc279a745c4ffd8298c917db15a776
ManlyMarco <dummyaccx@gmail.com>
2019-02-12 21:04:28 +0100

    Changed wording of the maker "is trap" button

--- KK_BecomeTrap/BecomeTrapGui.cs
+++ KK_BecomeTrap/BecomeTrapGui.cs
@@ -39,7 +39,7 @@ namespace KK_BecomeTrap
         {
             if (MakerAPI.GetMakerSex() == 0)
             {
-                _toggleEnabled = e.AddControl(new MakerToggle(MakerConstants.Parameter.Character, "Character is a trap", this));
+                _toggleEnabled = e.AddControl(new MakerToggle(MakerConstants.Parameter.Character, "Character is a trap or a futa (changes gameplay)", this));
                 _toggleEnabled.ValueChanged.Subscribe(val => GetMakerController().IsTrap = val);
             }
         }
