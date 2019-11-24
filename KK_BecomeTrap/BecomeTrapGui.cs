3570bfca5f222ac9aa97c3ad8be38bc410fa42c3
ManlyMarco <dummyaccx@gmail.com>
2019-11-24 12:48:41 +0100

    Update gui code to latest kkapi

--- KK_BecomeTrap/BecomeTrapGui.cs
+++ KK_BecomeTrap/BecomeTrapGui.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using KKAPI.Chara;
 using KKAPI.Maker;
 using KKAPI.Maker.UI;
-using UniRx;
 using UnityEngine;
 
 namespace KK_BecomeTrap
@@ -21,39 +19,11 @@ namespace KK_BecomeTrap
             new KeyValuePair<string, string>("Stand_13_01", "Ladylike")
         };
 
-        private static MakerDropdown _animType;
-        private static MakerToggle _toggleEnabled;
-
         internal static string DefaultIdleAnimation => _idleAnimations[0].Key;
 
         internal static void Initialize()
         {
             MakerAPI.RegisterCustomSubCategories += RegisterCustomSubCategories;
-            MakerAPI.ChaFileLoaded += (sender, args) => BecomeTrap.Instance.StartCoroutine(ChaFileLoadedCo());
-            MakerAPI.MakerExiting += MakerExiting;
-        }
-
-        private static IEnumerator ChaFileLoadedCo()
-        {
-            yield return null;
-
-            if (_toggleEnabled != null)
-            {
-                var ctrl = GetMakerController();
-                _toggleEnabled.Value = ctrl.IsTrap;
-                _animType.Value = Mathf.Max(0, _idleAnimations.FindIndex(pair => pair.Key == ctrl.IdleAnimation));
-            }
-        }
-
-        private static BecomeTrapController GetMakerController()
-        {
-            return MakerAPI.GetCharacterControl().gameObject.GetComponent<BecomeTrapController>();
-        }
-
-        private static void MakerExiting(object sender, EventArgs e)
-        {
-            _toggleEnabled = null;
-            _animType = null;
         }
 
         private static void RegisterCustomSubCategories(object sender, RegisterSubCategoriesEvent e)
@@ -62,11 +32,15 @@ namespace KK_BecomeTrap
             {
                 var category = MakerConstants.Parameter.Character;
 
-                _toggleEnabled = e.AddControl(new MakerToggle(category, "Character is a trap or a futa (changes gameplay)", BecomeTrap.Instance));
-                _toggleEnabled.ValueChanged.Subscribe(val => GetMakerController().IsTrap = val);
+                var isTrap = e.AddControl(new MakerToggle(category, "Character is a trap or a futa (changes gameplay)", BecomeTrap.Instance));
+                isTrap.BindToFunctionController<BecomeTrapController, bool>(
+                    (controller) => controller.IsTrap,
+                    (controller, value) => controller.IsTrap = value);
 
-                _animType = e.AddControl(new MakerDropdown("Idle trap animation", _idleAnimations.Select(x => x.Value).ToArray(), category, 0, BecomeTrap.Instance));
-                _animType.ValueChanged.Subscribe(i => GetMakerController().IdleAnimation = _idleAnimations[i].Key);
+                var animType = e.AddControl(new MakerDropdown("Idle trap animation", _idleAnimations.Select(x => x.Value).ToArray(), category, 0, BecomeTrap.Instance));
+                animType.BindToFunctionController<BecomeTrapController, int>(
+                    (controller) => Mathf.Max(0, _idleAnimations.FindIndex(pair => pair.Key == controller.IdleAnimation)),
+                    (controller, value) => controller.IdleAnimation = _idleAnimations[value].Key);
             }
         }
     }
