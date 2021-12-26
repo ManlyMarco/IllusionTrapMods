fb05ee414cb9c18375128d0f32ea692e857a141c
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2021-12-26 17:56:22 +0100

    [BT] Combine shared code; Update KK packages

---
+++ Core_BecomeTrap/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using KK_BecomeTrap;
+
+[assembly: AssemblyTitle("BecomeTrap")]
+[assembly: AssemblyDescription("Can turn male characters into playable traps")]
+[assembly: AssemblyCompany("https://github.com/ManlyMarco/IllusionTrapMods")]
+[assembly: AssemblyProduct("BecomeTrap")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyVersion(BecomeTrap.Version)]
